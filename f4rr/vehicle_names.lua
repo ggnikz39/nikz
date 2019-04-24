cd143c77f8b11963e8983cf1c268ee942136c2c2
ggnikz39 <49959870+ggnikz39@users.noreply.github.com>
2019-04-25 06:54:49 +0700

    batmanpack

---
+++ f4rr/vehicle_names.lua
@@ -0,0 +1,11 @@
+Citizen.CreateThread(function()
+	--   d99
+	AddTextEntry('5DE2D196', 'Remove Mirrors')
+	AddTextEntry('71EB14DD', 'Exhaust 2')
+	AddTextEntry('6400C100', 'Ducati')
+	AddTextEntry('AC77E7C8', 'Remove plate holder')
+	AddTextEntry('AE1C1ACC', 'Race mgaurd')
+	AddTextEntry('B2E46D3B', 'Ducati Racer')
+	AddTextEntry('C7D3D73D', '1199')
+	AddTextEntry('D27B55FC', 'Exhaust 1')
+  end)
