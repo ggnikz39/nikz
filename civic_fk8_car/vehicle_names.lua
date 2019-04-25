124ec6669096aec63661baa580cae3b61f467ee3
ggnikz39 <49959870+ggnikz39@users.noreply.github.com>
2019-04-25 10:03:59 +0700

    fk8

---
+++ civic_fk8_car/vehicle_names.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+	--   civic_fk8
+	AddTextEntry('5BFA7CED', 'Windows Paint2')
+	AddTextEntry('38A0B63A', 'Windows Paint4')
+	AddTextEntry('72EFD5A1', 'Modulo')
+	AddTextEntry('97F15D25', '2018 Civic Type R(FK8)')
+	AddTextEntry('253E3CFB', 'Modulo')
+	AddTextEntry('543CE560', 'Remove wing')
+	AddTextEntry('8723D32F', 'Windows Paint3')
+	AddTextEntry('54208D03', 'Varis Hood')
+	AddTextEntry('E94DEBAA', 'Varis  ')
+  end)
