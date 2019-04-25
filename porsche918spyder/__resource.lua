cb6e807f9a2d9c5a2ee43063661235b87ee155f3
ggnikz39 <49959870+ggnikz39@users.noreply.github.com>
2019-04-25 17:43:22 +0700

    Update __resource.lua

--- porsche918spyder/__resource.lua
+++ porsche918spyder/__resource.lua
@@ -6,7 +6,7 @@ files {
     'data/carcols.meta',
     'data/handling.meta',
 	'data/dlctext.meta',
-	'data/vehiclelayouts.meta',
+	'data/vehiclelayouts_918.meta',
 }
  
 data_file 'HANDLING_FILE' 'data/handling.meta'
@@ -14,6 +14,6 @@ data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
 data_file 'TEXTFILE_METAFILE' 'data/dlctext.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_918.meta'
 
 client_script 'vehicle_names.lua'
